183b8d132e2e86f33eb88a598402c8c83e90f748
SK <55817746+SKCro@users.noreply.github.com>
2024-06-28 00:20:13 -0400

    spice up intro

--- js/index.js
+++ js/index.js
@@ -7,11 +7,11 @@ function typesim(newsng,target){
 	//stolen from brew.rocks
 	const delay=ms => new Promise(res => setTimeout(res, ms));
 	let i=0;
-	let typ=function(){
-		return new Promise(resolve => {
+	function typ(){
+		return new Promise(resolve=>{
 			function typeWriter(){
 				if(i<=target.innerHTML.length){
-					target.innerHTML += newsng.charAt(i);
+					target.innerHTML+=newsng.charAt(i);
 					i++;
 					setTimeout(typeWriter, typespeed);
 				}else{
@@ -28,28 +28,40 @@ function typesim(newsng,target){
 }
 
 const introSequence=async function(){
+	document.querySelector('.loading-text').remove();
+	document.querySelector('.skip-text').classList.add('shown');
+	const blinkingCursor=document.createElement('span');
+	blinkingCursor.classList.add('blinking-cursor');
+	blinkingCursor.textContent='_';
+	terminal.appendChild(blinkingCursor);
 	await sleep(1500);
+
 	if(cutsceneSkipped===true){return;}
-	document.getElementById('cursorFlash').remove();
-	var c=document.createElement('span');
-	c.innerHTML='CS-DOS 9.20<br>Copyright (c) 1991, 1999 Chronospace, Inc. All rights reserved.';
-	terminal.appendChild(c);
+	document.querySelector('.blinking-cursor').remove();
+	const initText=document.createElement('span');
+	initText.innerHTML='CS-DOS 9.20<br>Copyright (c) 1991, 1999 Chronospace, Inc. All rights reserved.<br>';
+	terminal.appendChild(initText);
+	terminal.appendChild(blinkingCursor);
 	await sleep(1000);
 
-	var d=document.createElement('span');
-	d.id='terminalText1';
-	d.innerHTML=`<br><br>C:\&gt;`;
-	terminal.appendChild(d);
+	document.querySelector('.blinking-cursor').remove();
+	const firstCmd=document.createElement('span');
+	firstCmd.id='firstCmd';
+	firstCmd.innerHTML=`<br>C:\&gt;`;
+	terminal.appendChild(firstCmd);
+	terminal.appendChild(blinkingCursor);
 	await sleep(1025);
-	typesim("cd WINDOWSILL", document.querySelector("#terminalText1"));
+	typesim('cd WINDOWSILL',document.getElementById('firstCmd'));
 	await sleep(1500);
 
-	var e=document.createElement("span");
-	e.id='terminalText2';
-	e.innerHTML=`<br>C:\WINDOWSILL&gt;`;
-	terminal.appendChild(e);
+	document.querySelector('.blinking-cursor').remove();
+	const secondCmd=document.createElement("span");
+	secondCmd.id='secondCmd';
+	secondCmd.innerHTML=`<br>C:\WINDOWSILL&gt;`;
+	terminal.appendChild(secondCmd);
+	terminal.appendChild(blinkingCursor);
 	await sleep(800);
-	typesim("WINDOWSILL.COM", document.querySelector("#terminalText2"));
+	typesim('WINDOWSILL.COM',document.getElementById('secondCmd'));
 	await sleep(1500);
 
 	terminal.remove();
