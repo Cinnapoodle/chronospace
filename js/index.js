e55ca08d669c400c9ed5698b0f9b3c430f3781ea
SK <55817746+SKCro@users.noreply.github.com>
2024-06-27 23:17:32 -0400

    revamp styles + other stuff

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 const sleep=(delay) => new Promise((resolve) => setTimeout(resolve, delay));
-const console=document.getElementById('console');
+const terminal=document.getElementById('terminal');
 const typespeed=80;
+let cutsceneSkipped=false;
 
-function typesim(newsng, songtxt, del){
+function typesim(newsng,target){
 	//stolen from brew.rocks
 	const delay=ms => new Promise(res => setTimeout(res, ms));
 	let i=0;
 	let typ=function(){
 		return new Promise(resolve => {
 			function typeWriter(){
-				if (i <= songtxt.innerHTML.length){
-					songtxt.innerHTML += newsng.charAt(i);
-					i=i + 1;
+				if(i<=target.innerHTML.length){
+					target.innerHTML += newsng.charAt(i);
+					i++;
 					setTimeout(typeWriter, typespeed);
 				} else {
 					i=0;
@@ -30,35 +31,37 @@ function typesim(newsng, songtxt, del){
 
 const introSequence=async function(){
 	await sleep(1500);
+	if(cutsceneSkipped===true){return;}
 	document.getElementById('cursorFlash').remove();
 	var c=document.createElement('span');
 	c.innerHTML='CS-DOS 9.20<br>Copyright (c) 1991, 1999 Chronospace, Inc. All rights reserved.';
-	console.appendChild(c);
+	terminal.appendChild(c);
 	await sleep(1000);
 
 	var d=document.createElement('span');
-	d.id='consoletext1';
-	d.innerHTML='<br><br>' + 'C:\\>';
-	console.appendChild(d);
+	d.id='terminalText1';
+	d.innerHTML=`<br><br>C:\&gt;`;
+	terminal.appendChild(d);
 	await sleep(1025);
-	typesim("cd WINDOWSILL", document.querySelector("#consoletext1"), false);
+	typesim("cd WINDOWSILL", document.querySelector("#terminalText1"));
 	await sleep(1500);
 
 	var e=document.createElement("span");
-	e.id='consoletext2';
-	e.innerHTML='<br>' + 'C:\\WINDOWSILL>';
-	console.appendChild(e);
+	e.id='terminalText2';
+	e.innerHTML=`<br>C:\WINDOWSILL&gt;`;
+	terminal.appendChild(e);
 	await sleep(800);
-	typesim("WINDOWSILL.COM", document.querySelector("#consoletext2"), false);
+	typesim("WINDOWSILL.COM", document.querySelector("#terminalText2"));
 	await sleep(1500);
 
-	console.remove();
+	terminal.remove();
 	await sleep(800);
 	$(".screen-center").fadeIn(1000);
 }
 
 function skipCutscene(){
+	cutsceneSkipped=true;
 	$(".screen-center").fadeIn(1000);
-	console.remove();
+	terminal.remove();
 }
 addEventListener('DOMContentLoaded',introSequence);
